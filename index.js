d7343f40d621bba1e6dcaeb2b897a393f937a6ea
Vladimir Semenov <thesun2003@gmail.com>
2016-10-16 19:28:24 +1300

    commented out mongodb connection

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
  
-var api = require('./Server/routes')
+var api = require('./server/routes')
 
-mongoose.connect('mongodb://localhost:27017/test');
+// mongoose.connect('mongodb://localhost:27017/test');
 
 app.use('/api', api);
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
