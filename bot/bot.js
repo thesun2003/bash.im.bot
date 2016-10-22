fa1a3a5c663ec499b411b2dfa1c1e20e301a1bcc
Vladimir Semenov <thesun2003@gmail.com>
2016-10-22 21:36:07 +1300

    Trying different things;

--- bot/bot.js
+++ bot/bot.js
@@ -22,10 +22,12 @@ bot.on('message', (payload, reply) => {
   bot.getProfile(payload.sender.id, (err, profile) => {
     if (err) throw err
 
+      reply({ text: 'Getting a new qoute... this might take a few seconds.' })
+
       get_random_quote()
         .then((result) => {
 
-          reply({ result }, (err) => {
+          reply({ text: result }, (err) => {
             if (err) throw err
 
             console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${result}`)
