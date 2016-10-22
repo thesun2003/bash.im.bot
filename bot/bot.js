9a7f5117db297e52b722fb92fb56f2b2e0f1b7f7
Vladimir Semenov <thesun2003@gmail.com>
2016-10-22 21:22:35 +1300

    Trying to send back a Quote;

--- bot/bot.js
+++ bot/bot.js
@@ -22,11 +22,14 @@ bot.on('message', (payload, reply) => {
   bot.getProfile(payload.sender.id, (err, profile) => {
     if (err) throw err
 
+    reply({ text }, (err) => {
+      if (err) throw err
+
     get_random_quote()
-      .then( (result) => {
+      .then(function(result) {
         reply({ result }, (err) => {
           if (err) throw err
-    })
+    });
 
       console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
     })
